a8a9ebd90919e8dfe9a62cdcd00af9db0a0a78b1
riktothepast <rik@rik-ThinkPad-T430s.(none)>
2013-06-20 21:03:25 -0600

    added mm1 solving method

--- src/methods/AbstractMethod.java
+++ src/methods/AbstractMethod.java
@@ -4,6 +4,8 @@ public abstract class AbstractMethod {
 	float lambda;
 	float mu;
 	float ro;
+	float n;
+	float k;
 	
 	public abstract float getW();
 	public abstract float getWq();
@@ -11,12 +13,17 @@ public abstract class AbstractMethod {
 	public abstract float getLq();
 	public abstract float getPo();
 	public abstract float getPn();
+	public abstract float getRo();
+
 	
 	public AbstractMethod(){}
 	public float getLambda(){return this.lambda;}
-	public float getRo(){return this.ro;}
 	public float getMu(){return this.mu;}
+	public float getN(){return this.n;}
+	public float getK(){return this.k;}
 	public void setLambda(float l){this.lambda=l;}
 	public void setRo(float ro){this.ro=ro;}
 	public void setMu(float m){this.mu=m;}
+	public void setN(float n){this.n=n;}
+	public void setK(float n){this.k=n;}
 }
---
+++ src/methods/Mm1.java
@@ -0,0 +1,59 @@
+package methods;
+
+public class Mm1 extends AbstractMethod {
+
+	public void setValues(float lambda, float mu, float n,float ro){
+		setLambda(lambda);
+		setMu(mu);
+		setN(n);
+		setRo(ro);
+	}
+
+	@Override
+	public float getW() {
+		return 1/(getMu()-getLambda());
+	}
+
+	@Override
+	public float getWq() {
+		return getLambda()/(getMu()*(getMu()-getLambda()));
+	}
+
+	@Override
+	public float getL() {
+		return getLambda()/(getMu()-getLambda());
+	}
+
+	@Override
+	public float getLq() {
+		return getLambda()*getWq();
+	}
+
+	@Override
+	public float getPo() {
+		if(getMu()!=0&&getLambda()!=0){
+			return (getMu()-getLambda())/getMu();
+		}
+		return 1-getRo();
+	}
+
+	@Override
+	public float getPn() {
+		if(getMu()!=0&&getLambda()!=0){
+			float part1,part2;
+			part1=(float) Math.pow((getLambda()/getMu()),getN());
+			part2=(getMu()-getLambda())/getMu();
+			return part1*part2;
+		}
+		return ((float) Math.pow(getRo(), getN()))*(1-getRo());
+	}
+
+	@Override
+	public float getRo() {
+		if(getMu()!=0&&getLambda()!=0){
+			return getLambda()/getMu();
+		}
+		return this.ro;
+	}
+
+}
